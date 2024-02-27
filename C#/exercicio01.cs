da2f474d7bd793a468dd130b60acb2ff884ba933
Gabriela Paulini Deutner <deutnerg@gmail.com>
2024-02-26 21:50:22 -0300

    estudos c#

---
+++ C#/exercicio01.cs
@@ -0,0 +1,16 @@
+/*
+1. Criar uma variável chamada curso, e guarde nela o nome do curso estudado.
+2. Mostrar o conteúdo da variável curso no console.
+3. Criar uma variável chamada nome e outra chamada sobrenome e guardar nelas seu nome e sobrenome.
+4. Mostrar na tela o nome dos instrutores do curso.
+*/
+
+string curso = "C#: criando sua primeira aplicação";
+
+Console.WriteLine = (curso);
+
+string nome = "Gabriela";
+string sobrenome = "Deutner";
+
+Console.WriteLine("Daniel Portugal e Gui Lima");
+
