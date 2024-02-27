bd0d496ccb13d731f849acf64c04bb60c5ff8016
Gabriela Paulini Deutner <deutnerg@gmail.com>
2024-02-27 17:10:52 -0300

    exericios c#

---
+++ C#/exercicio04.cs
@@ -0,0 +1,64 @@
+/*
+Escrever uma função que a partir de dois números de ponto flutuante a e b exiba no console o resultado de suas quatro 
+operações básicas (adição, subtração, divisão e multiplicação), utilizando interpolação de strings.
+Criar uma lista de bandas vazia e adicionar suas bandas prediletas em seguida.
+Utilizar a estrutura 'for' para mostrar todas as suas bandas preferidas, listadas na lista do exercício anterior, 
+no console.
+Criar um programa que calcula a soma de todos os elementos inteiros em uma lista.
+*/
+
+// Primeiro exercício
+
+void ExibirQuatroOperacoes(){
+
+float a = 4;
+float b = 8;
+
+float soma = a + b;
+float subtracao = a - b;
+float divisao = a / b;
+float multiplicacao = a * b;
+
+Console.WriteLine($"a + b = {soma}");
+Console.WriteLine($"a - b = {subtracao}");
+Console.WriteLine($"a / b = {divisao}");
+Console.WriteLine($"a * b = {multiplicacao}");
+}
+
+ExibirQuatroOperacoes();
+
+
+
+
+// Segundo exercício
+
+List<string> bandas = new List<string>();
+
+bandas.Add("Lacuna Coil");
+bandas.Add("Metallica");
+bandas.Add("Tash Sultana");
+
+
+// terceiro exercicio
+for(int i = 0; i < bandas.Count; i++){
+    Console.WriteLine(bandas[i]);
+}
+
+
+// Quarto exercicio
+
+List<int> elementos = new List<int>();
+int soma = 0;
+
+bandas.Add(1);
+bandas.Add(2);
+bandas.Add(3);
+bandas.Add(4);
+bandas.Add(5);
+
+foreach (int numero in numeros)
+{
+    soma = soma + numero;
+}
+
+Console.WriteLine($"A soma dos elementos da lista é: {soma}");
