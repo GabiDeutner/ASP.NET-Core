9670051c9f923959c47a9a36aeb48d48849f490e
Gabriela Paulini Deutner <deutnerg@gmail.com>
2024-02-27 09:46:45 -0300

    treinando c#

--- C#/exercicio02.cs
+++ C#/exercicio02.cs
@@ -7,6 +7,67 @@ Criar um programa que, dado a entrada de dados de um número n inteiro, a partir
 de uma lista.
 */
 
+
+// Minha Resolução:
+
+// primeiro desafio
+int notaMedia = 5;
+
+if (notaMedia >= 5) {
+    Console.WriteLine("Nota suficiente para aprovação");
+} else {
+    Console.WriteLine("Nota insuficiente para aprovação");
+}
+
+
+
+
+// Segundo Desafio
+// Criando uma lista de string
+List<string> Linguagens = new List<string>();
+
+// Adicionando elementos à lista
+Linguagens.Add("C#");
+Linguagens.Add("Java");
+Linguagens.Add("JavaScript");
+
+// Exibir o valor C#
+Console.WriteLine(Linguagens[0]);
+
+
+
+
+
+// Terceiro Desafio
+// Escolhendo a enésima posição
+
+// Criando uma lista de string
+List<string> Linguagens = new List<string>();
+
+// Adicionando elementos à lista
+Linguagens.Add("C#");
+Linguagens.Add("Java");
+Linguagens.Add("JavaScript");
+
+Console.WriteLine("Escolha uma posição: ");
+string num = Console.ReadLine();
+int n = int.Parse(num);
+Console.WriteLine(Linguagens[n]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Resolução Alura
+
 int media = 5;
 if(media >= 5){
     Console.WriteLine("Nota suficiente para aprovação");
